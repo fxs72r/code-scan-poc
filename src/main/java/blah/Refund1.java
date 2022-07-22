86ad149831f966576b536b3b9161c4b29d69cd9f
Frank <frank.skrtic@sherwin.com>
2022-07-22 01:17:53 -0400

    code scannning

--- src/main/java/blah/Refund1.java
+++ src/main/java/blah/Refund1.java
@@ -6,6 +6,7 @@ public class Refund1 {
   private String tenderCategory_ = null;
   
   protected String checkVisibilityImpl(Object[] tenders) throws Exception {
+    String unusedLocal;
     if (this.tenderCategory_ != null) {
       boolean anyAvailable = false;
       for (Object tndr : tenders) {
