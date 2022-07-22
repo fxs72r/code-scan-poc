28e831ed9d230992f483aefcf31637ea576cf7d1
Frank <frank.skrtic@sherwin.com>
2022-07-22 00:56:16 -0400

    code scannning

---
+++ src/blah/Refund1.java
@@ -0,0 +1,28 @@
+
+package blah;
+
+public class Refund1 {
+
+  private String tenderCategory_ = null;
+  
+  protected String checkVisibilityImpl(Object[] tenders) throws Exception {
+    if (this.tenderCategory_ != null) {
+      boolean anyAvailable = false;
+      for (Object tndr : tenders) {
+        Object tender2 = Refund2.getOtherTender();
+        if (tender2 != null) {
+          anyAvailable = true;
+          break;
+        }
+        if (anyAvailable) 
+          continue;
+        return Refund2.DENIED;
+      }
+    }
+  return Refund2.GRANTED;
+  }
+
+}
+
+
+
---
+++ src/blah/Refund2.java
@@ -0,0 +1,21 @@
+package blah;
+
+public class Refund2 {
+
+    public static String passsword = "a_fake_password1$";
+
+    public static String DENIED = "DENIED";
+
+    public static String GRANTED = "GRANTED";
+
+    public static Object other;
+
+    public static Object getOtherTender() {
+      return other;
+    }
+
+    public static void setOtherTender(Object o) {
+      other = o;
+    }
+
+}
